57d431d45a8a9de51c7c222d152827ce3ee14076
wachuleru <redhot088@gmail.com>
2022-08-27 15:32:26 -0400

    se agregan servicios

---
+++ src/app/dbz/personajes/personajes.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
+
+@Component({
+  selector: 'app-personajes',
+  templateUrl: './personajes.component.html',
+  styleUrls: ['./personajes.component.css']
+})
+export class PersonajesComponent  {
+
+  //@Input('data') personajes:Personaje[]=[];
+
+  get personajes(){
+    return this.dbzService.personajes;
+  }
+
+  constructor(private dbzService: DbzService){
+
+  }
+}
---
+++ src/app/dbz/services/dbz.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from "@angular/core";
+import { Personaje } from "../interfaces/dbz.interface";
+
+@Injectable()
+export class DbzService {
+
+    private _personajes: Personaje[]=[
+        {
+          nombre:'Goku',
+          poder: 15000
+        },
+        {
+          nombre: 'Vegeta',
+          poder: 10000
+        },
+        {
+          nombre: 'Mr Satan',
+          poder: 1
+        }
+      ];
+    
+    get personajes (): Personaje[]{
+        return [...this._personajes];
+    }
+    
+
+    constructor(){
+    }
+
+    agregarPersonaje( personaje: Personaje){
+        this._personajes.push( personaje );
+    }
+}
